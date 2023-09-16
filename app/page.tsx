a12029e89c5ed4aa63b217e5a461c39ee7430d09
GiangLe1999 <legiangbmt09@gmail.com>
2023-09-16 09:38:58 +0700

    Fix bug for new deployment

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ import { getCarDataByCategory } from "@/lib/fetchData";
 import { CarByCategory } from "@/types";
 
 export default async function Home() {
-  // const cars = (await getCarDataByCategory()) as CarByCategory[];
+  const cars = (await getCarDataByCategory()) as CarByCategory[];
   return (
     <main className="mt-[61px] space-y-14">
       <HomeSwiper />
-      {/* <CarCategories cars={cars} /> */}
+      <CarCategories cars={cars} />
       <div className="">Hello</div>
     </main>
   );
